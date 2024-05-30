1f27932640ff10329cabc3ca197d7413a3fef9c0
zhangyuzhu111 <1580006194@qq.com>
2024-05-30 18:40:30 +0800

    chore:logo

---
+++ projects/client/src/components/logo/Logo.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import * as echarts from 'echarts'
+
+type EChartsOptions = echarts.EChartsOption
+type ECharts = echarts.ECharts
+
+const el = <HTMLDivElement | null>(null)
+let instance: ECharts | null = null
+
+function renderChart() {
+  const option: EChartsOptions = {
+    graphic: {
+    elements: [
+        {
+          type: 'text',
+          left: 'center',
+          top: 'center',
+          style: {
+            text: 'Apache ECharts',
+            fontSize: 80,
+            fontWeight: 'bold',
+            lineDash: [0, 200],
+            lineDashOffset: 0,
+            fill: 'transparent',
+            stroke: '#000',
+            lineWidth: 1
+          },
+          keyframeAnimation: {
+            duration: 3000,
+            loop: false,
+            keyframes: [
+              {
+                percent: 0.7,
+                style: {
+                  fill: 'transparent',
+                  lineDashOffset: 200,
+                  lineDash: [200, 0]
+                }
+              },
+              {
+                percent: 0.8,
+                style: {
+                  fill: 'transparent'
+                }
+              },
+              {
+                percent: 1,
+                style: {
+                  fill: 'black'
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+  instance?.setOption(option)
+}
+
+onMounted(() => {
+  instance = echarts.init(el)
+  renderChart
+})
+</script>
+
+<template>
+  <div ref="el" full />
+</template>
