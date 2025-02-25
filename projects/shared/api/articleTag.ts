89a618aa5a36ea412a6a2108580605c585062ac7
zhangyuzhu <1580006194@qq.com>
2025-02-25 15:04:47 +0800

    feat(server):新增文章标签相关接口

--- projects/shared/api/articleTag.ts
+++ projects/shared/api/articleTag.ts
@@ -1,5 +1,5 @@
 import type {
-  IArticleType,
+  IArticleTag,
   IIdsDto,
   IQueryDto,
   IQueryPaginatedResData,
@@ -7,45 +7,38 @@ import type {
 } from 'types'
 import { useRequest } from '../composables/request'
 
-const { $get, $post, $patch, $delete } = useRequest()
+const { $post, $patch, $delete } = useRequest()
 
 /**
- * 获取文章分类列表
+ * 查询文章标签列表
  */
-export function getArticleTypeListApi() {
-  return $get<IArticleType[]>('/article/type/list')
+export function queryArticleTagListApi(body: IQueryDto<IArticleTag>) {
+  return $post<IQueryPaginatedResData<IArticleTag>>('/article/tag/query', body)
 }
 
 /**
- * 查询文章分类列表
- */
-export function queryArticleTypeListApi(body: IQueryDto<IArticleType>) {
-  return $post<IQueryPaginatedResData<IArticleType>>('/article/type/query', body)
-}
-
-/**
- * 添加文章分类
+ * 添加文章类别
  */
 export function createArticleTypeApi(body: IUpsertArticleTypeBodyDto) {
   return $post<string>('/article/type/create', body)
 }
 
 /**
- * 编辑文章分类
+ * 编辑文章类别
  */
 export function updateArticleTypeApi(body: IUpsertArticleTypeBodyDto, articleTypeId: string) {
   return $patch<string>(`/article/type/update/${articleTypeId}`, body)
 }
 
 /**
- * 删除文章分类
+ * 删除文章类别
  */
 export function deleteArticleTypeApi(body: IIdsDto) {
   return $delete<number>('/article/type/delete', body)
 }
 
 /**
- * 删除指定文章分类
+ * 删除指定文章类别
  */
 export function deleteArticleTypeByIdApi(articleTypeId: string) {
   return $delete<number>(`/article/type/delete/${articleTypeId}`)
