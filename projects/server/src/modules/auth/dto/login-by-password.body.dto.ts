7b6dd9c0f8dfa4f419ff8514d71de901226a0c1b
zhangyuzhu <zhangyuzhu0177@163.com>
2025-09-12 09:19:03 +0800

    chore(server):添加过期时间swagger文档

--- projects/server/src/modules/auth/dto/login-by-password.body.dto.ts
+++ projects/server/src/modules/auth/dto/login-by-password.body.dto.ts
@@ -5,6 +5,8 @@ import { PasswordDto } from 'src/dto/password.dto'
 import { EmailOptionalDto } from 'src/dto/email.dto'
 import { CodeVerifyDto } from 'src/dto/code-verify.dto'
 import { AccountOptionalDto } from 'src/dto/account.dto'
+import { ApiPropertyOptional } from '@nestjs/swagger'
+import { IsNumber, IsOptional } from 'class-validator'
 
 export class LoginByPasswordBodyDto
   extends Mixin(
@@ -14,6 +16,11 @@ export class LoginByPasswordBodyDto
     CodeVerifyDto,
   )
   implements ILoginByPasswordBodyDto {
-  /** 过期时间戳 */
-  expires?: number
+  @ApiPropertyOptional({
+    description: '过期时间 (秒)',
+    example: 3600,
+  })
+  @IsNumber()
+  @IsOptional()
+  expiresIn?: number
 }
